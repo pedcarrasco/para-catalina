53125f6f60d36743699cead04f264f6b821ee47b
Pedro Carrasco <ped.carrasco@duocuc.cl>
2026-07-23 23:20:07 -0400

    Fix vite config syntax

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,7 @@
-import { defineConfig } from 'vite';`nimport react from '@vitejs/plugin-react';`n`nexport default defineConfig({`n  plugins: [react()],`n  base: '/para-catalina/',`n});
+import { defineConfig } from 'vite'; 
+import react from '@vitejs/plugin-react'; 
+ 
+export default defineConfig({ 
+  plugins: [react()], 
+  base: '/para-catalina/', 
+}); 
